05d2dfac78aebb1ea89d6d2769aad03657a59712
Latira <l@g.com>
2025-03-08 08:41:02 +0300

    update: third task complete

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -60,8 +60,10 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = Object.assign(obj);
+  keys.forEach((key) => delete result[key]);
+  return result;
 }
 
 /**
